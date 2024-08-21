981ec48b9700dd11028cb10bd550097bf485796c
GianlucaA18 <imanol.milla@gmail.com>
2024-08-21 11:59:04 -0500

    beneficios

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Banner from "@/components/clientes/banner";
+import Beneficios from "@/components/clientes/beneficios";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <Beneficios />
     </div>
   );
 }
