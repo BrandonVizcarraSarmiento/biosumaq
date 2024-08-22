8d1c24be9017cd6d79111bcf366189e105a5ef7e
GianlucaA18 <imanol.milla@gmail.com>
2024-08-22 10:41:53 -0500

    Testimonios

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Banner from "@/components/clientes/banner";
 import MisionVision from "@/components/clientes/misionVision";
 import Beneficios from "@/components/clientes/beneficios";
 import Image from "next/image";
+import Testimonios from "@/components/clientes/testimonios";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Banner />
       <MisionVision />
       <Beneficios />
+      <Testimonios />
     </div>
   );
 }
---
+++ components/clientes/testimonios.tsx
@@ -0,0 +1,64 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Terminal } from "lucide-react";
+
+
+const Testimonios = () => {
+    return (
+        <section>
+            <h2 className="text-center text-3xl font-bold mb-6">Testimonios</h2>
+        <div>
+          <Alert className="flex items-start bg-blue-100 p-4 rounded-lg mb-4">
+            <Avatar className="mr-4">
+              <AvatarImage src="/img/iconoPerfil.jpg" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div>
+              <Terminal className="h-4 w-4" />
+              <AlertTitle>Heads up!</AlertTitle>
+              <AlertDescription>
+                You can add components and dependencies to your app using the CLI.
+              </AlertDescription>
+            </div>
+          </Alert>
+        </div>
+      
+        <div>
+          <Alert className="flex items-start bg-blue-100 p-4 rounded-lg mb-4">
+            <Avatar className="mr-4">
+              <AvatarImage src="/img/iconoPerfil.jpg" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div>
+              <Terminal className="h-4 w-4" />
+              <AlertTitle>Heads up!</AlertTitle>
+              <AlertDescription>
+                You can add components and dependencies to your app using the CLI.
+              </AlertDescription>
+            </div>
+          </Alert>
+        </div>
+      
+        <div>
+          <Alert className="flex items-start bg-blue-100 p-4 rounded-lg mb-4">
+            <Avatar className="mr-4">
+              <AvatarImage src="/img/iconoPerfil.jpg" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div>
+              <Terminal className="h-4 w-4" />
+              <AlertTitle>Heads up!</AlertTitle>
+              <AlertDescription>
+                You can add components and dependencies to your app using the CLI.
+              </AlertDescription>
+            </div>
+          </Alert>
+        </div>
+      </section>
+      
+      
+      
+    );
+}
+
+export default Testimonios;
