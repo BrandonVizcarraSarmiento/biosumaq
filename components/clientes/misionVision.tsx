23c7c372681dbd9f1e7b3d59c87c7b772112767e
Brandon <brandon.vizcarra@sfact.pe>
2024-08-22 09:09:24 -0500

    vision

--- components/clientes/misionVision.tsx
+++ components/clientes/misionVision.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 const MisionVision = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-4 justify-items-center">
             {/* Columna 1 */}
             <div className="flex flex-col items-center text-center space-y-2">
-                <Image src="/img/mision.svg" alt="imagen de misión" width={300} height={300} className="w-full max-w-xs" />
-                <div className="p-4 max-w-sm">
+                <Image src="/img/mision.svg" width={300} height={300} alt="imagen de misión" className="h-64 w-64 mx-auto" />
+                <div className="max-w-sm mx-auto">
                     <p className="text-sm">
                         En Fishfood, nuestra misión es ofrecer productos alimenticios de pescado de la más alta calidad, que sean saludables, deliciosos y accesibles para todos. Nos comprometemos a innovar en el desarrollo de opciones prácticas y nutritivas, respetando siempre los recursos marinos y promoviendo una alimentación equilibrada.
                     </p>
@@ -14,10 +14,10 @@ const MisionVision = () => {
             </div>
 
             {/* Columna 2 */}
-            <div className="flex flex-col items-center text-center space-y-2">
-                <Image src="/img/vision.svg" alt="imagen de visión" width={300} height={300} className="w-full max-w-xs" />
-                <div className="p-4 max-w-sm">
-                    <p className="text-sm">
+            <div className="flex flex-col justify-center items-center text-center space-y-2">
+                <Image src="/img/vision.svg" width={300} height={300} alt="imagen de visión" className="h-64 w-64 mx-auto" />
+                <div className="max-w-sm mx-auto">
+                    <p className="text-sm my-4">
                         Ser la empresa líder en el mercado de productos preformados a base de pescado, reconocida por nuestra excelencia en calidad, sostenibilidad y por contribuir a una alimentación más sana en todo el mundo.
                     </p>
                 </div>
--- components/clientes/redes.tsx
+++ components/clientes/redes.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Redes = () => {
     return (
-        <div className="fixed right-4 top-1/2 transform -translate-y-1/2 flex flex-col items-center space-y-5 p-2 rounded-lg shadow-lg">
+        <div className="fixed right-4 top-1/2 transform -translate-y-1/2 flex flex-col items-center space-y-5 p-2 rounded-lg shadow-lg z-10">
             <Link href="/"><Image src="/img/whatsapp.svg" width={30} height={30} alt={"Imagen de whatsapp"} className=""/></Link>
             <Link href="/"><Image src="/img/instagram.svg" width={30} height={30} alt={"Imagen de instagram"} /></Link>
             <Link href="/"><Image src="/img/facebook.svg" width={30} height={30} alt={"Imagen de facebook"} /></Link> 
