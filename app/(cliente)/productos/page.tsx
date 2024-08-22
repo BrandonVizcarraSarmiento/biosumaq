49b99e0d1d3b6da04cda8e8b33f3e3f2f829bd0d
Brandon <brandon.vizcarra@sfact.pe>
2024-08-22 11:48:48 -0500

    productos

--- app/(cliente)/productos/page.tsx
+++ app/(cliente)/productos/page.tsx
@@ -6,20 +6,23 @@ const SeccionProductos = () => {
             <section className="flex flex-col items-center p-4">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl w-full">
                     {/* Columna izquierda: imagen grande */}
-                    <div className="col-span-2 flex items-center">
+                    <div className="col-span-1 md:col-span-2 flex justify-center items-center">
                         <Productos />
                     </div>
 
                     {/* Columna derecha: imágenes pequeñas */}
-                    <div className="flex flex-col justify-between space-y-4">
-                        <div className="flex-1 flex items-start">
+                    <div className="col-span-1 flex flex-col justify-between space-y-4">
+                        <div className="flex-1 flex justify-center items-start">
                             <Productos />
                         </div>
-                        <div className="flex-1 flex items-end">
+                        <div className="flex-1 flex justify-center items-end">
                             <Productos />
                         </div>
                     </div>
                 </div>
+            </section>
+            <section>
+
             </section>
         </div>
     );
