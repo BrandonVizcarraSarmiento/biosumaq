3e72d6142de8c9a241623f7e6995731b6adef6c5
Brandon <brandon.vizcarra@sfact.pe>
2024-08-29 10:48:59 -0500

    novedades cambios

--- app/(cliente)/novedades/components/eventoPrincipal.tsx
+++ app/(cliente)/novedades/components/eventoPrincipal.tsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
-import ProductoNombrePrecio from "../../productos/components/productoNombrePrecio";
+import FechaEvento from "./fecha";
 
 const EventoPrincipal = () => {
-    return (
-        <div className="relative w-full max-w-md mx-auto">
-        <img src="/img/feritac.avif" alt="Feritac" className="w-full h-auto" />
-        <h2 className="absolute inset-0 flex items-center justify-center text-white text-2xl md:text-4xl font-bold drop-shadow-md">
-          Evento Feritac
-        </h2>
-        <span className="absolute bottom-2 left-2 text-white text-sm md:text-base drop-shadow-md">
-          29/08/2024
-        </span>
+  return (
+    <div className="w-full max-w-6xl mx-auto px-4">
+      <div className="relative rounded-md border-2 border-transparent hover:border-cyan-400 transition-colors duration-300">
+        <Link href="/" className="block">
+          <Image src="/img/feritac.avif" layout="responsive" width={300} height={300} alt="Imagen de producto" className="transition-transform duration-400 transform hover:scale-110" />
+          <div className="absolute bottom-2 left-2">
+            <FechaEvento />
+          </div>
+        </Link>
       </div>
-      
-    );
+    </div>
+  );
 }
 
 export default EventoPrincipal;
