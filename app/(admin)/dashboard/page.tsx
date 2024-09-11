a515178b64d3863276efe679ed005904b5864eb8
Brandon <brandon.vizcarra@sfact.pe>
2024-09-11 10:34:47 -0500

    carpetas dashboard

---
+++ app/(admin)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+    return (
+      <div>
+        <h2>Bienvenido al Dashboard</h2>
+        <p>Este es el contenido del dashboard.</p>
+      </div>
+    );
+  }
