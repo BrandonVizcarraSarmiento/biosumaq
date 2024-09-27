2f2b58b8d31030c8bc99cd6776567b0e29b31ba6
Brandon <brandon.vizcarra@sfact.pe>
2024-09-27 17:29:30 -0500

    img

--- app/(routes)/(home)/novedades/components/eventoPrincipal.tsx
+++ app/(routes)/(home)/novedades/components/eventoPrincipal.tsx
@@ -1,32 +1,27 @@
-import Image from "next/image";
 import Link from "next/link";
 import FechaEvento from "./fecha";
 
 const EventoPrincipal = () => {
   return (
     <div className="w-full max-w-6xl mx-auto px-4 my-10">
-    <div className="relative rounded-md border-2 border-gray-50 hover:border-black transition-colors duration-300 dark:border-black dark:hover:border-white">
-      <Link href="/infonovedades" className="block">
-        <div className="relative">
-          <Image
-            src="/img/feritac.avif"
-            layout="responsive"
-            width={300}
-            height={300}
-            alt="Imagen de producto"
-            className="transition-transform duration-400 transform hover:scale-110 rounded-md brightness-50"
-          />
-          <h2 className="absolute inset-0 flex items-center justify-center text-white text-3xl md:text-5xl font-bold drop-shadow-md">
-            Evento más reciente
-          </h2>
-        </div>
-        <div className="absolute bottom-2 left-2">
-          <FechaEvento />
-        </div>
-      </Link>
+      <div className="relative rounded-md border-2 border-gray-50 hover:border-black transition-colors duration-300 dark:border-black dark:hover:border-white">
+        <Link href="/infonovedades" className="block">
+          <div className="relative">
+            <img
+              src="/img/feritac.avif"
+              alt="Imagen de producto"
+              className="w-full h-auto rounded-md brightness-50" // Asegúrate de que la imagen ocupe el 100% del contenedor
+            />
+            <h2 className="absolute inset-0 flex items-center justify-center text-white text-3xl md:text-5xl font-bold drop-shadow-md">
+              Evento más reciente
+            </h2>
+          </div>
+          <div className="absolute bottom-2 left-2">
+            <FechaEvento />
+          </div>
+        </Link>
+      </div>
     </div>
-  </div>
-  
   );
 }
 
