c160e9dafed23a1886d5fad6e527c5d9b2607788
GianlucaA18 <imanol.milla@gmail.com>
2024-10-24 09:34:08 -0500

    margin about mobile

--- components/clientes/menuMobile.tsx
+++ components/clientes/menuMobile.tsx
@@ -8,11 +8,11 @@ const MenuMobile = () => {
             <PopoverTrigger className="p-2 rounded-full hover:bg-gray-200 transition-all">
                 <Menu className="h-6 w-6 text-gray-700" />
             </PopoverTrigger>
-            <PopoverContent className="bg-white rounded-lg shadow-lg p-4 space-y-2">
-                <Link href="/" className="block text-lg font-medium text-gray-700 hover:text-blue-500 transition-colors">Inicio</Link>
-                <Link href="/about" className="block text-lg font-medium text-gray-700 hover:text-blue-500 transition-colors">Sobre nosotros</Link>
-                <Link href="/productos" className="block text-lg font-medium text-gray-700 hover:text-blue-500 transition-colors">Productos</Link>
-                <Link href="/novedades" className="block text-lg font-medium text-gray-700 hover:text-blue-500 transition-colors">Novedades</Link>
+            <PopoverContent className=" rounded-lg shadow-lg p-4 space-y-2">
+                <Link href="/" className="block text-lg font-medium   hover:text-blue-500 transition-colors">Inicio</Link>
+                <Link href="/about" className="block text-lg font-medium  hover:text-blue-500 transition-colors">Sobre nosotros</Link>
+                <Link href="/productos" className="block text-lg font-medium  hover:text-blue-500 transition-colors">Productos</Link>
+                <Link href="/novedades" className="block text-lg font-medium  hover:text-blue-500 transition-colors">Novedades</Link>
             </PopoverContent>
         </Popover>
     );
